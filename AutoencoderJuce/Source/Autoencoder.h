0c05ad9dfe1755385a78f0fa6a58766e998542ea
Joaquin Romera <joakromera@gmail.com>
2020-11-07 20:40:57 -0300

    small changes

--- AutoencoderJuce/Source/Autoencoder.h
+++ AutoencoderJuce/Source/Autoencoder.h
@@ -22,21 +22,22 @@
 
 class Autoencoder
 {
-    struct parameters;
-    struct slider;
+    struct Parameters;
+    struct SliderMinMax;
 
 public:
 
-    explicit Autoencoder(const std::string& pModel, const parameters &pParams)
+    explicit Autoencoder(const std::string& pModel, const Parameters &pParams)
         : mAutoencoder(fdeep::read_model_from_string(pModel, false, nullptr))
         , mInput(mAutoencoder.get_input_shapes()[0].depth_.unsafe_get_just(), 0.0f)
         , mTensors { fdeep::tensor(fdeep::tensor_shape{mInput.size()}, mInput) }
         , mParams(pParams)
-        , rfft_size(mAutoencoder.get_output_shapes()[0].depth_.unsafe_get_just())
-        , mask(mParams.win_length - 1)
+        , rfftSize(mAutoencoder.get_output_shapes()[0].depth_.unsafe_get_just())
+        , fftArray {}
+        , mask(mParams.winLength - 1)
         , index(0)
-        , idx_proc(0)
-        , mAudio(mParams.win_length, 0.0f)
+        , idxProc(0)
+        , mAudio(mParams.winLength, 0.0f)
         , random {}
         , mFFT {12}
     {
@@ -63,17 +64,12 @@ public:
         nlohmann::json settings;
         ifs >> settings;
 
-        parameters params;
-        params.win_length = settings["parameters"]["win_length"];
-        params.sClip = std::stof(settings["parameters"]["sClip"].get<std::string>());
-        params.xMax = std::stof(settings["parameters"]["xMax"].get<std::string>());
-        for (const auto &i : settings["parameters"]["zRange"])
-        {
-            params.zRange.emplace_back(
-                std::stof(i["min"].get<std::string>()),
-                std::stof(i["max"].get<std::string>())
-            );
-        }
+        Parameters params = {
+            std::stof(settings["parameters"]["xMax"].get<std::string>()),
+            std::stof(settings["parameters"]["sClip"].get<std::string>()),
+            settings["parameters"]["win_length"],
+            settings["parameters"]["zRange"]
+        };
 
         return std::make_unique<Autoencoder>(settings["model"].dump(), params);
     }
@@ -125,60 +121,70 @@ public:
     {
         const auto predictionResult = mAutoencoder.predict(mTensors)[0].as_vector();
 
-        for (int i = 0; i < rfft_size; ++i)
+        for (int i = 0; i < rfftSize; ++i)
         {
             const float power = ((predictionResult->at(i) * mParams.xMax) + mParams.sClip) / 10;
             const float y_aux = std::sqrt(std::pow(10, power));
             const float phase = random.nextFloat() * juce::MathConstants<float>::twoPi;
-            fft_array[2 * i] = y_aux * std::cos(phase);
-            fft_array[(2 * i) + 1] = y_aux * std::sin(phase);
+            fftArray[2 * i] = y_aux * std::cos(phase);
+            fftArray[(2 * i) + 1] = y_aux * std::sin(phase);
         }
 
-        mFFT.performRealOnlyInverseTransform(fft_array);
+        mFFT.performRealOnlyInverseTransform(fftArray);
 
-        for (int i = 0; i < mParams.win_length; ++i)
+        for (int i = 0; i < mParams.winLength; ++i)
         {
             const float multiplier = 0.5f * (
-                1 - std::cos(juce::MathConstants<float>::twoPi * i / (mParams.win_length - 1))
+                1 - std::cos(juce::MathConstants<float>::twoPi * i / (mParams.winLength - 1))
             );
-            const float sample = fft_array[i] * multiplier;
-            const int idx = (idx_proc + i) & mask;
+            const float sample = fftArray[i] * multiplier;
+            const int idx = (idxProc + i) & mask;
             mAudio[idx] += sample;
         }
 
-        idx_proc += bufferToFillSize;
-        idx_proc &= mask;
+        idxProc += bufferToFillSize;
+        idxProc &= mask;
     }
 
 private:
 
-    struct slider
+    struct SliderMinMax
     {
-        slider(float pMin, float pMax) : min(pMin), max(pMax) {}
-
         float min;
         float max;
     };
 
-    struct parameters
+    struct Parameters
     {
+        Parameters(float pXMax, float pSClip, const unsigned int pWinLength, const nlohmann::json &PZRange)
+            : xMax(pXMax)
+            , sClip(pSClip)
+            , winLength(pWinLength)
+        {
+            for (const auto &r : PZRange)
+                zRange.push_back({
+                    std::stof(r["min"].get<std::string>()),
+                    std::stof(r["max"].get<std::string>())
+                });
+        }
+
         float xMax;
         float sClip;
-        unsigned win_length;
-        std::vector<slider> zRange;
+        const unsigned winLength;
+        std::vector<SliderMinMax> zRange;
     };
 
     const fdeep::model mAutoencoder;
     std::vector<float> mInput;
     fdeep::tensors mTensors;
 
-    parameters mParams;
-    const unsigned rfft_size;
+    Parameters mParams;
+    const unsigned rfftSize;
 
-    float fft_array[4096*2] = {};
+    float fftArray[4096 * 2];
     const int mask;
     int index;
-    int idx_proc;
+    int idxProc;
     std::vector<float> mAudio;
 
     juce::Random random;
